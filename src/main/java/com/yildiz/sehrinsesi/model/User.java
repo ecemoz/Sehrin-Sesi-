7ba412afbc8603834115279de540f01d44c7e76b
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-03-04 14:40:37 +0300

    user model is finished.

--- src/main/java/com/yildiz/sehrinsesi/model/User.java
+++ src/main/java/com/yildiz/sehrinsesi/model/User.java
@@ -1,4 +1,37 @@
 package com.yildiz.sehrinsesi.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id" , referencedColumnName = "id")
+    private Adress address;
 }
