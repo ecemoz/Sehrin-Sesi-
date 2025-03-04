98b19eb0b7823e7f6de0cbd4c9de71820f15d126
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-03-04 14:19:35 +0300

    Project is created.

---
+++ src/main/java/com/yildiz/sehrinsesi/SehrinSesiApplication.java
@@ -0,0 +1,13 @@
+package com.yildiz.sehrinsesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SehrinSesiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SehrinSesiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yildiz/sehrinsesi/SehrinSesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yildiz.sehrinsesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SehrinSesiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
