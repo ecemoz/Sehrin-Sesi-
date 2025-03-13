47c75496e108dcb90d064fd20676599c0f9bae63
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-03-13 16:33:55 +0300

    UserCreateDTO is created.

---
+++ src/main/java/com/yildiz/sehrinsesi/dto/UserCreateDTO.java
@@ -0,0 +1,17 @@
+package com.yildiz.sehrinsesi.dto;
+
+import com.yildiz.sehrinsesi.utils.UserRole;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserCreateDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private UserRole userRole;
+    private String phoneNumber;
+}
