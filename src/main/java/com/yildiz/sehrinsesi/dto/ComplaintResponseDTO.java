7819c146fdb9d24e08d312c8d58b941d489f8fe5
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-03-22 02:43:49 +0300

    ComplaintResponseDTO is finished.

--- src/main/java/com/yildiz/sehrinsesi/dto/ComplaintResponseDTO.java
+++ src/main/java/com/yildiz/sehrinsesi/dto/ComplaintResponseDTO.java
@@ -1,4 +1,20 @@
 package com.yildiz.sehrinsesi.dto;
 
+import com.yildiz.sehrinsesi.utils.ComplaintsStatus;
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class ComplaintResponseDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private double latitude;
+    private double longitude;
+    private LocalDateTime createdAt;
+    private ComplaintsStatus status;
+    private String username;
 }
